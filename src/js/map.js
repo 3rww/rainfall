b42b14f5134b85fcffdb3aaeb8e680662a6fa35f
Christian Gass <christiangass@outlook.com>
2018-05-07 16:37:07 -0400

    data processing

--- src/js/map.js
+++ src/js/map.js
@@ -1,6 +1,8 @@
 var $ = (jQuery = require("jquery"));
 var d3 = require("d3");
+var _ = require("lodash");
 var L = require("leaflet");
+var moment = require("moment");
 topojson = require("topojson-client");
 
 var url =
@@ -21,10 +23,70 @@ function d31() {
     )
   );
 
+
+  /**
+   * store rainfall tallies from the Rainfall API
+   * @param {*} data 
+   */
+  var rainfallTally = class {
+    /**
+     * pass in the initial API response, and pre-calculate some things
+     */
+    constructor(data) {
+      // store the data
+      this.data = data;
+      // populate a list of times
+      this.times = Object.keys(this.data);
+      // total rainfall per cell      
+      this.total = {}
+      // accumulation per cell
+      this.accumulation = {}
+
+      // stub out the accumulation property with all cell ids, each set to zero
+      Object.keys(this.data[Object.keys(this.data)[0]]).map(x => {
+        this.accumulation[x] = 0
+        this.total[x] = 0
+      })
+
+      // calculate total rainfal per cell basis
+      this._tally(this.data);
+    }
+    /**
+     * helper method, applies mergeWith with an additive customizer function
+     * @param {*} source 
+     * @param {*} plus 
+     */
+    _merger(source, plus) {
+      return _.mergeWith(source, plus, function (objValue, srcValue) {
+        return objValue + srcValue;
+      })
+    }
+    /**
+     * calculate total accumulation per cell. Effectively, this does what accumulator does, except 
+     * all at once and ahead of time.
+     * @param {*} data 
+     */
+    _tally(data) {
+      return _.each(data, (v, k) =>
+        this._merger(this.total, v)
+      )
+    }
+    /**
+     * Increment rainfall total by cell ID, passing in a new rainfall response object for a given time.
+     * Updates the accumulation property
+     */
+    accumulator(addThis) {
+      return this._merger(this.accumulation, addThis)
+    }
+  };
+
+
   const radigeography = d3
     .scaleSqrt()
     .domain([0, 2])
-    .range([0, 5]);
+    .range([0, 15]);
+
+  const stretch = d3.scaleLinear()
 
   // add an svg element to the Leaflet Map's Overlay Pane
   var svg_map = d3.select(map.getPanes().overlayPane).append("svg");
@@ -33,6 +95,19 @@ function d31() {
   var transform = d3.geoTransform({
     point: projectPoint
   });
+
+  // d3geoPath
+  var gridAsPath = d3.geoPath().projection(transform);
+  // grid container
+  var g = svg_map
+    .append("g")
+    .attr("id", "grid")
+    .attr("class", "leaflet-zoom-hide");
+  // circle container
+  var g2 = svg_map
+    .append("g")
+    .attr("id", "dots")
+    .attr("class", "leaflet-zoom-hide");
 
   /**
    * Use Leaflet to implement a D3 geometric transformation.
@@ -64,66 +139,66 @@ function d31() {
   }
 
   /**
-   * add the grid to the map as a D3 element
+   * transforms the rainfall API response for a single point in time into a Map object
+   * @param {*} data rainfall API response
    */
-  function addGrid(geojson, data) {
-    // sample from timeseries test data
-    var rainfallobj = [];
+  function transformRainfallResponse(data) {
     var d = data;
+    var rainfallobj = [];
     // console.log("adding...", geojson, d);
-    $.each(d, function (i, v) {
+    $.each(d, function (k, v) {
       rainfallobj.push({
-        id: i,
+        id: k,
         rain: v
       });
     });
     console.log(rainfallobj);
     var rainfall = new Map(rainfallobj.map(d => [d.id, d.rain]));
-    console.log(rainfall);
+    // console.log(rainfall);
+    return rainfall
+  }
 
-    // d3geoPath
-    var gridAsPath = d3.geoPath().projection(transform);
+  /**
+   * add the grid to the map as a D3 element
+   */
+  function addGrid(geojson, rainfall) {
 
     // append a g element to the svg element - this is where data will go
-    var g = svg_map
-      .append("g")
-      .attr("id", "grid")
-      .attr("class", "leaflet-zoom-hide");
+
+    var data = geojson.features
+      .map(d => {
+        var x = ((d.properties.rain = rainfall.get(d.properties.id)), d);
+        // console.log(x)
+        if (!x.properties.rain) {
+          x.properties.rain = 0;
+        }
+        return x;
+      })
+      .sort((a, b) => b.properties.id - a.properties.id)
 
     var gridcell = g
       .selectAll("path")
-      .data(geojson.features)
+      .data(data)
       .enter()
       .append("path")
+      .attr("stroke", "#ccc")
+      .attr("stroke-width", 0.5)
+      .attr("fill-opacity", 0)
+      // .attr("fill", d3.interpolateYlGnBu(t)
       .attr("id", d => d.properties.id)
       .attr("class", "mapgridcell");
 
-    var g2 = svg_map
-      .append("g")
-      .attr("id", "dots")
-      .attr("class", "leaflet-zoom-hide");
-
     var circle = g2
       .selectAll("circle")
-      .data(
-        geojson.features
-        .map(d => {
-          var x = ((d.properties.rain = rainfall.get(d.properties.id)), d);
-          if (!x.rain) {
-            x.rain = 0.000001;
-          }
-          return x;
-        })
-        .sort((a, b) => b.id - a.id)
-      )
+      .data(data)
       .enter()
       .append("circle")
-      .attr("id", d => d.id)
-      // .attr("transform", d => `translate(${gridAsPath.centroid(d)})`)
+      .attr("id", d => d.properties.id)
       .attr("transform", d => `translate(${makeCentroid(gridAsPath, d)})`)
       .attr("r", d => radigeography(d.properties.rain))
-      .attr("fill", "#3890e2")
-      .attr("fill-opacity", 0.8)
+      .attr("stroke", "#3890e2")
+      .attr("stroke-width", 0.5)
+      .attr("fill-opacity", 0)
       .append("title")
       .text(d => `${d.properties.rain} inches | ${d.properties.watershed} | ${d.properties.ww_basin} `);
 
@@ -139,129 +214,101 @@ function d31() {
      * Recalculate bounds for redrawing *grid* each time map changes
      */
     function reset() {
-      console.log("reset");
+      // console.log("reset");
+
+      // calculate bounds
       var bounds = gridAsPath.bounds(geojson);
       var topLeft = bounds[0];
       var bottomRight = bounds[1];
-      // var topLeft = [bounds[0][0] + 10, bounds[0][1] - 10];
-      // var bottomRight = [bounds[1][0] + 10, bounds[1][1] + 10];
-
       svg_map
         .attr("width", bottomRight[0] - topLeft[0])
         .attr("height", bottomRight[1] - topLeft[1])
         .style("left", topLeft[0] + "px")
         .style("top", topLeft[1] + "px");
 
+      // calculate translation
       var gTranslate = "translate(" + -topLeft[0] + "," + -topLeft[1] + ")";
-      console.log(gTranslate);
+      // appy translation to the grid
       g.attr("transform", gTranslate);
-
       gridcell.attr("d", gridAsPath);
 
-      g2.attr("transform", gTranslate);
-      // g2.attr("transform", function(d) {
-      //   if (d) {
-      //     return (
-      //       "translate(" +
-      //       applyLatLngToLayer(d).x +
-      //       "," +
-      //       applyLatLngToLayer(d).y +
-      //       ")"
-      //     );
-      //   }
-      // });
+      // apply translation to the circles
       g2
+        .attr("transform", gTranslate)
         .selectAll("circle")
         .attr("transform", d => `translate(${makeCentroid(gridAsPath, d)})`);
-
       circle.attr("d", gridAsPath);
     }
   }
 
-  /**
-  function addData(geojson, data) {
-    // sample from timeseries test data
-    var rainfallobj = [];
-    var d = data["2004-09-17T13:00:00"];
-    console.log("adding...", geojson, d);
-    $.each(d, function(i, v) {
-      rainfallobj.push({
-        id: i,
-        rain: v
-      });
-    });
-    var rainfall = new Map(rainfallobj.map(d => [d.id, d.rain]));
-    console.log(rainfall);
+  function updateData(geojson, rainfall) {
+    var data = geojson.features
+      .map(d => {
+        var x = ((d.properties.rain = rainfall.get(d.properties.id)), d);
+        if (!x.rain) {
+          x.rain = 0.000001;
+        }
+        return x;
+      })
+      .sort((a, b) => b.properties.id - a.properties.id)
 
-    var g2 = svg_map
-      .append("g")
-      .attr("class", "leaflet-zoom-hide")
-      .attr("fill", "blue")
-      .attr("fill-opacity", 0.2)
-      // .attr("stroke", "#fff")
-      // .attr("stroke-width", 0.1)
+    var t = d3.transition()
+      .duration(100)
+      .ease(d3.easeLinear);
+
+    var circle = g2
       .selectAll("circle")
-      .data(
-        geojson.features
-          .map(d => {
-            var x = ((d.rain = rainfall.get(d.id)), d);
-            if (!x.rain) {
-              x.rain = 1;
-            }
-            // console.log(x);
-            return x;
-          })
-          .sort((a, b) => b.id - a.id)
-        // topojson
-        //   .feature(geography, geography.objects)
-        //   .features.map(d => ((d.population = population.get(d.id)), d))
-        //   .sort((a, b) => b.id - a.id)
-      )
-      .enter()
-      .append("circle")
-      .attr("id", d => d.id)
-      .attr("transform", d => `translate(${gridAsPath.centroid(d)})`)
-      .attr("r", d => radigeography(d.rain))
-      .append("title")
-      .text(d => d.id);
+      .data(data)
+      // .transition(t)
+      .attr("transform", d => `translate(${makeCentroid(gridAsPath, d)})`)
+      .attr("r", d => radigeography(d.properties.rain))
+      .text(d => `${d.properties.rain} inches | ${d.properties.watershed} | ${d.properties.ww_basin} `);
 
-    map.on("zoomend", resetData);
-    // map.on("moveend", reset);
-
-    // reset the view
-    resetData();
-
-    // Recalculate bounds for redrawing *grid* each time map changes
-    function resetData() {
-      var bounds = gridAsPath.bounds(geojson);
-      var topLeft = bounds[0];
-      var bottomRight = bounds[1];
-      console.log("reset", bounds, topLeft, bottomRight);
-
-      svg_map
-        .attr("width", bottomRight[0] - topLeft[0])
-        .attr("height", bottomRight[1] - topLeft[1])
-        .style("left", topLeft[0] + "px")
-        .style("top", topLeft[1] + "px");
-
-      g2.attr(
-        "transform",
-        "translate(" + -topLeft[0] + "," + -topLeft[1] + ")"
-      );
-
-      g2.attr("d", gridAsPath);
-    }
-
+    return "complete";
   }
-  */
+
 
   // get grid and add it
   d3.json("http://localhost:3000/data/grid.geojson", function (geojson) {
-    d3.json("http://localhost:3000/data/test4.json", function (data) {
-      // addGrid(geojson, data["2004-09-17T06:00:00"]);
-      addGrid(geojson, data["2016-08-28T19:00:00"]);
+    d3.json("http://localhost:3000/data/test7.json", function (data) {
+      // addGrid(geojson, transformRainfallResponse(data["2016-08-28T19:00:00"]));
+
+      var tally = new rainfallTally(data);
+      console.log(tally);
+
+      console.log("starting at", Object.keys(data)[0]);
+      updateTimestamp(Object.keys(data)[0]);
+      addGrid(geojson, transformRainfallResponse(
+        data[Object.keys(data)[0]]));
+
+      var i = 0;
+
+      function doUpdate() {
+        i++;
+        if (i >= Object.keys(data).length) {
+          return
+        };
+        setTimeout(function () {
+          var now = Object.keys(data)[i];
+          console.log(now)
+          $("#timestamp").html(now);
+          updateTimestamp(now);
+          updateData(geojson, transformRainfallResponse(data[now]))
+          doUpdate();
+        }, 100)
+      }
+
+      doUpdate();
+
     });
   });
+
+
+}
+
+function updateTimestamp(timestamp) {
+  var t = moment(timestamp).format("dddd, MMMM Do YYYY, h:mm:ss a");
+  $("#timestamp").html(t)
 }
 
 // run this map-making function
